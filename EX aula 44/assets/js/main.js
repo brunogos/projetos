63271b8784fdb3c317b2670ba28642f7f7662f88
Bruno Gomes Da Silva <bruno.g.s@outlook.com.br>
2022-04-24 15:26:14 -0300

    alteração

--- EX aula 44/assets/js/main.js
+++ EX aula 44/assets/js/main.js
@@ -3,7 +3,23 @@ const form = document.querySelector('#formulario'); // como é um id utiliza ('#
 
 form.addEventListener('submit', function (e) { //para parar o envio do formulario utilizamos a função (form.addEventListener)
   e.preventDefault();
-  console.log('Evento previnido.');
-  
+  const inputPeso = e.target.querySelector('#peso');
+  const inputAltura = e.target.querySelector('#altura');
+
+  const peso = Number(inputPeso.value);
+  const altura = Number(inputAltura.value);
+  console.log(peso, altura);
 });
+
+function criaP () {
+  const p = document.createElement('p'); // Criar uma variavel - criando um paragrafo ('p')
+  return p;
+}
+
+function setResultado (msg) {
+  const resultado = document.querySelector('#resultado');
+  resultado.innerHTML = '';
+
+  const p = criaP();
+}
  
